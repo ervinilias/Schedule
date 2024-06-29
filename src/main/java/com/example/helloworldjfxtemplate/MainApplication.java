c464b1780cbaa62d726dad3251eaf6572a6e1b03
LabUser <eiliasov@yahoo.com>
2024-06-29 15:25:31 -0600

    Commit 1

--- src/main/java/com/example/helloworldjfxtemplate/MainApplication.java
+++ src/main/java/com/example/helloworldjfxtemplate/MainApplication.java
@@ -13,7 +13,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Main View!");
+        stage.setTitle("Main");
         stage.setScene(scene);
         stage.show();
     }
