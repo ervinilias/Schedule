94256ed62387dafbc566ce471045aaaf36b2fd28
LabUser <eiliasov@yahoo.com>
2024-06-29 18:24:36 -0600

    Commit 1

--- src/main/java/com/example/helloworldjfxtemplate/controller/LoginController.java
+++ src/main/java/com/example/helloworldjfxtemplate/controller/LoginController.java
@@ -1,11 +1,26 @@
 package com.example.helloworldjfxtemplate.controller;
 
+import com.example.helloworldjfxtemplate.DAO.UserDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class LoginController {
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.time.ZoneId;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     @FXML
     private Button btn_exit;
 
@@ -13,13 +28,13 @@ public class LoginController {
     private Button btn_login;
 
     @FXML
-    private Label lbl_intro;
+    private Label lbl_location;
 
     @FXML
-    private Label lbl_intro1;
+    private Label lbl_userloc;
 
     @FXML
-    private Label lbl_intro11;
+    private Label lbl_name;
 
     @FXML
     private Label lbl_password;
@@ -28,13 +43,57 @@ public class LoginController {
     private Label lbl_username;
 
     @FXML
-    void setBtn_login(ActionEvent event) {
+    private TextField tf_password;
 
+    @FXML
+    private TextField tf_username;
+
+    ResourceBundle langBundle = ResourceBundle.getBundle("lang");
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ZoneId zoneId = ZoneId.systemDefault();
+        String location = ZoneId.systemDefault().toString();
+        lbl_name.setText(langBundle.getString("SchedulingApp"));
+        lbl_location.setText(langBundle.getString("Location"));
+        lbl_userloc.setText(location);
+        lbl_username.setText(langBundle.getString("Username"));
+        lbl_password.setText(langBundle.getString("Password"));
+        btn_login.setText(langBundle.getString("Login"));
+        btn_exit.setText(langBundle.getString("Exit"));
+
+    }
+
+    @FXML
+    void setBtn_login(ActionEvent event) throws IOException, SQLException {
+        String username = tf_username.getText();
+        String password = tf_password.getText();
+
+        if(username.isBlank() || username.isEmpty()) {
+
+        } else if (password.isBlank() || password.isEmpty()) {
+
+        } else if(!UserDAO.userValid(username)) {
+
+        } else if(!UserDAO.passValid(password)) {
+
+        } else if(UserDAO.userLogin(username,password)) {
+            new FXMLLoader();
+            Parent parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("menu.fxml")));
+            Scene scene = new Scene(parent);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+
+        }
     }
 
     @FXML
     void setBtn_exit(ActionEvent event) {
 
     }
+
+
+
 
 }
--- src/main/java/com/example/helloworldjfxtemplate/helper/Error.java
+++ src/main/java/com/example/helloworldjfxtemplate/helper/Error.java
@@ -1,4 +1,47 @@
 package com.example.helloworldjfxtemplate.helper;
 
+import javafx.scene.control.Alert;
+import java.util.ResourceBundle;
+
 public class Error {
+    static ResourceBundle langBundle = ResourceBundle.getBundle("lang");
+
+    public static void getError(int numError) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        switch (numError){
+            case 1:
+                alert.setTitle(langBundle.getString("ErrorBlankUser"));
+                alert.setContentText(langBundle.getString("blankUser"));
+                alert.showAndWait();
+                break;
+
+            case 2:
+                alert.setTitle(langBundle.getString("ErrorBlankPass"));
+                alert.setContentText(langBundle.getString("blankPass"));
+                alert.showAndWait();
+                break;
+
+            case 3:
+                alert.setTitle(langBundle.getString("ErrorUsername"));
+                alert.setContentText(langBundle.getString("incorrectUsername"));
+                alert.showAndWait();
+                break;
+
+            case 4:
+                alert.setTitle(langBundle.getString("ErrorPass"));
+                alert.setContentText(langBundle.getString("incorrectPass"));
+                alert.showAndWait();
+                break;
+
+            case 5:
+                alert.setTitle(langBundle.getString("ErrorUserPass"));
+                alert.setContentText(langBundle.getString("incorrectUserPass"));
+                alert.showAndWait();
+                break;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + numError);
+
+        }
+    }
 }
