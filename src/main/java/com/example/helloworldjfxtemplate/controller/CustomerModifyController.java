dbc31c02cb0a2d9f3f5d9166500789690e6229c1
LabUser <eiliasov@yahoo.com>
2024-07-01 14:10:33 -0600

    Commit 1

---
+++ src/main/java/com/example/helloworldjfxtemplate/controller/CustomerModifyController.java
@@ -0,0 +1,51 @@
+package com.example.helloworldjfxtemplate.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class CustomerModifyController {
+    @FXML
+    private Button btn_cancel;
+
+    @FXML
+    private Button btn_save;
+
+    @FXML
+    private ComboBox<?> cb_custCountry;
+
+    @FXML
+    private ComboBox<?> cb_custDivision;
+
+    @FXML
+    private TextField tf_custAddr;
+
+    @FXML
+    private TextField tf_custID;
+
+    @FXML
+    private TextField tf_custName;
+
+    @FXML
+    private TextField tf_custPhone;
+
+    @FXML
+    private TextField tf_custPost;
+
+    @FXML
+    void setBtn_cancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setBtn_save(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tf_cust(ActionEvent event) {
+
+    }
+}
---
+++ src/main/java/com/example/helloworldjfxtemplate/controller/CustomersController.java
@@ -0,0 +1,61 @@
+package com.example.helloworldjfxtemplate.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
+public class CustomersController {
+    @FXML
+    private Button btn_addCust;
+
+    @FXML
+    private Button btn_delCust;
+
+    @FXML
+    private Button btn_menu;
+
+    @FXML
+    private Button btn_updtCust;
+
+    @FXML
+    private TableColumn<?, ?> col_custAddr;
+
+    @FXML
+    private TableColumn<?, ?> col_custCountry;
+
+    @FXML
+    private TableColumn<?, ?> col_custID;
+
+    @FXML
+    private TableColumn<?, ?> col_custName;
+
+    @FXML
+    private TableColumn<?, ?> col_custPhone;
+
+    @FXML
+    private TableColumn<?, ?> col_custPost;
+
+    @FXML
+    private TableColumn<?, ?> col_custState;
+
+    @FXML
+    void setBtn_addCust(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setBtn_delCust(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setBtn_menu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setBtn_updtCust(ActionEvent event) {
+
+    }
+}
